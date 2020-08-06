ca0ec0069eaeaa0fe5da30b1394adf245b6783a4
mhmmtysil <muhammetyesil@outlook.com.tr>
2020-08-06 19:32:24 +0300

    MovementFinish

--- Assets/Scripts/CameraControll.cs
+++ Assets/Scripts/CameraControll.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class CameraControll : MonoBehaviour
 {
-    GameObject player;
     public Joystick joystick;
-    void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
 
     public float yOffset = 0;
     public float smoothRot = 2f;
     void LateUpdate()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z - 5);
+        //transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z - 5);
         yOffset += joystick.Horizontal * smoothRot;
         transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, yOffset, transform.rotation.eulerAngles.z));
         
