7dbb7927e6b03430c564e3ff77bdf7cf8b6cbe5b
mhmmtysil <muhammetyesil@outlook.com.tr>
2020-08-07 17:24:22 +0300

    Update AdConfiguration.cs

--- Assets/Scripts/AdConfiguration.cs
+++ Assets/Scripts/AdConfiguration.cs
@@ -36,7 +36,11 @@ public class AdConfiguration : MonoBehaviour
 
         // Create a 320x50 banner at the top of the screen.
         this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
-        
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
     }
 
     public void RequestInterstitial()
