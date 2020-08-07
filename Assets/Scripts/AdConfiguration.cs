72a5718b1888b4a3349c75d9f6029669b150b722
mhmmtysil <muhammetyesil@outlook.com.tr>
2020-08-07 22:47:21 +0300

    final

--- Assets/Scripts/AdConfiguration.cs
+++ Assets/Scripts/AdConfiguration.cs
@@ -6,6 +6,7 @@ using System;
 public class AdConfiguration : MonoBehaviour
 {
     public static AdConfiguration Instance { get; private set; }
+    private RewardBasedVideoAd rewardBasedVideo;
     BannerView bannerView;
     InterstitialAd interstitial;
     // Start is called before the first frame update
@@ -18,18 +19,58 @@ public class AdConfiguration : MonoBehaviour
         }
 
         MobileAds.Initialize(initStatus => { });
-
+        rewardBasedVideo = RewardBasedVideoAd.Instance;
+        rewardBasedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
         RequestBanner();
         DontDestroyOnLoad(gameObject);
         Instance = this;
+    }
+    public void RequestRewardBasedVideo()
+    {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-5390271097374264/9014237510"; //"ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-5390271097374264/5458135886";//"ca-app-pub-3940256099942544/1712485313";
+#else
+        string adUnitId = "unexpected_platform";
+#endif
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded video ad with the request.
+        this.rewardBasedVideo.LoadAd(request, adUnitId);
+    }
+
+    private void UserOptToWatchAd()
+    {
+        if (rewardBasedVideo.IsLoaded())
+        {
+            rewardBasedVideo.Show();
+        }
+    }
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        GameConfiguration.Instance.NextLevel();
+    }
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+    }
+
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        UserOptToWatchAd();
     }
 
     private void RequestBanner()
     {
 #if UNITY_ANDROID
-            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+            string adUnitId = "ca-app-pub-5390271097374264/5032961604"; //"ca-app-pub-3940256099942544/6300978111";
 #elif UNITY_IPHONE
-            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+            string adUnitId = "ca-app-pub-5390271097374264/4075103151"; //"ca-app-pub-3940256099942544/2934735716";
 #else
         string adUnitId = "unexpected_platform";
 #endif
@@ -46,9 +87,9 @@ public class AdConfiguration : MonoBehaviour
     public void RequestInterstitial()
     {
 #if UNITY_ANDROID
-        string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        string adUnitId =  "ca-app-pub-5390271097374264/6154471580";//"ca-app-pub-3940256099942544/1033173712";
 #elif UNITY_IPHONE
-        string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+        string adUnitId = "ca-app-pub-5390271097374264/9135858146"; //"ca-app-pub-3940256099942544/4411468910";
 #else
         string adUnitId = "unexpected_platform";
 #endif
@@ -56,10 +97,10 @@ public class AdConfiguration : MonoBehaviour
         this.interstitial = new InterstitialAd(adUnitId);
         this.interstitial.OnAdLoaded += HandleOnAdLoaded;
 
-        // Create an empty ad request.
-        AdRequest request = new AdRequest.Builder().Build();
-        // Load the interstitial with the request.
-        this.interstitial.LoadAd(request);
+         // Create an empty ad request.
+    AdRequest request = new AdRequest.Builder().Build();
+    // Load the interstitial with the request.
+    this.interstitial.LoadAd(request);
     }
 
     private void GameOver()
--- Assets/Scripts/GameConfiguration.cs
+++ Assets/Scripts/GameConfiguration.cs
@@ -37,6 +37,7 @@ public class GameConfiguration : MonoBehaviour
         new Word("TIGER", "Tigers are just BIG CATS! Don't try to Hug them!", 90f),
         new Word("MONKEY", "The Biggest Monkeys are Mandrel Monkeys. ", 90f),
         new Word("PENGUIN", "There are no penguins in the Northern Hemisphere, including the North Pole", 90f),
+        new Word("POLARBEAR", "Polar bear feeds on fish and seals"),
         new Word("SNAKE", "Snakes are legless but they can crawl and move ", 90f),
         new Word("BUTTERFLY", "Butterfly lives more than one day!", 90f),
         new Word("SPIDER", "There are 43.244 known species of Spiders. Wow!", 90f),
@@ -48,36 +49,35 @@ public class GameConfiguration : MonoBehaviour
         new Word("NORTHAMERICA", "There are 23 countries in North America", 90f),
         new Word("SOUTHAMERICA", "More than 371.000.000 people lives in South America.", 90f),
         new Word("AUSTRALIA", "The best known animal in Australia is kangaroo!!", 90f),
-        new Word("", "", 90f),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
-        new Word("", ""),
+        new Word("WATERMELON", "92% of Watermelon is Water!", 90f),
+        new Word("TOMATOES", "tomatoes are fruits!"),
+        new Word("POTATOES", "Potatoes contain starch"),
+        new Word("ORANGE", "Orange trees can live more than a hundred years."),
+        new Word("CUCUMBER", "Cucumbers grow in all climates except tundra.."),
+        new Word("STRAWBERRY", "grapes and strawberries are from the same family"),
+        new Word("HAZELNUT", "Turkey produces 85% of hazelnuts in the world"),
+        new Word("CABBAGE", "100 grams of cabbage is 110 kJ on average."),
+        new Word("APPLE", "1000 different apple varieties were produced by cultural studies."),
+        new Word("SPRING", "Nature is greenest in spring"),
+        new Word("WINTER", "Snow rains in Winter! Too cold!"),
+        new Word("SUMMER", "People usually go on vacation in summer"),
+        new Word("AUTUMN", "Trees fall leaves in autumn"),
+        new Word("GUITAR", "The guitar is played with fingers and picks. It's So good!!"),
+        new Word("KALIMBA", "It is a musical instrument born in South Africa."),
+        new Word("VUVUZELA", "vuvuzela is similar to elephant sound"),
+        new Word("PIANO", "There are 88 keys on the piano. It's so hard to memorize :)"),     
+        new Word("GROOM", "Groom marries the Bride"),
+        new Word("BRIDE", "Bride marries the Groom"),
+        new Word("OCEAN", "The deepest place of the marina pit is 11.030 meters. Don't fall!!"),
+        new Word("FOOTBALL", "There are over 113 thousand professional football players in the world."),
+        new Word("VOLLEYBALL", "Volleyball is played with 12 people."),
+        new Word("BASKETBALL", "The most scored points in a game in basketball is 320!!"),
+        new Word("MOBILEPHONE", "Mobile phone was first used in 1973"),
+        new Word("COMPASS", "The first compass in Europe began to be used in the 13th century."),
+        new Word("INTERNET", "the internet was invented in 1989."),
+        new Word("OXYGEN", "21% of the world is oxygen"),
+        new Word("REFORMGAMES", "MazEscape is created and Published by REFORMGAMES. :)"),
+        new Word("CONGRATULATIONS", "Congratulations! You Fınıshed the game! Check for Update! We are Working!"),
     };
     #endregion
     [HideInInspector]
@@ -188,6 +188,11 @@ public class GameConfiguration : MonoBehaviour
         ingame.SetActive(false);
         GameOver.SetActive(false);
         NextCanvas.SetActive(true);
+        if(levelNow+1 >= 49)
+        {
+            PlayerPrefs.DeleteAll();
+            levelNow = 0;
+        }
 
         txtAnswer.text = "Correct !The Answer is :" + words[levelNow].Name + "\n" + words[levelNow].Description;
     }
